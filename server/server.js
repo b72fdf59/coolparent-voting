8a97ad6c4e0c4e245f17481e598402d940be7b1a
logust79 <logust@yahoo.com>
2019-03-15 11:24:39 +0000

    add support for .env

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
-// @flow
+require("dotenv").config();
 const express = require("express");
-
-// bodyParser, parses the request body to be a readable json format
+const typeform = require("@typeform/api-client");
+const typeformAPI = typeform.createClient({
+  token: process.env.TYPEFORM_TOKEN
+});
 
 const app = express();
 
+app.get("/api", (_, res) => {
+  // will retrieve all forms
+  typeformAPI.responses.list({ uid: process.env.FORM_ID }).then(data => {
+    res.json({ success: true, data });
+  });
+});
+
+function scoring(data) {
+  // input: response data from typeform API
+  // output calculated scores for each parent
+  let students, parents;
+  for (let individual of data.items) {
+  }
+}
 app.get("/coolparent", (_, res) => {
   const maxInt = 30;
   const people = [
@@ -31,4 +47,4 @@ app.get("/coolparent", (_, res) => {
   return res.json({ success: true, data: data });
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
